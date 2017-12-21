5146abe89466097baec2133245f6e79b67881bc6
Feliphe Pozzer <feliphepozzer@gmail.com>
2017-12-21 16:30:17 -0200

    Criando as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :examples, only: :index do
+    collection do
+      post :upload_file
+    end
+  end
+  root to: "examples#index"
 end
